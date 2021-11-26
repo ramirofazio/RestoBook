35cd1998c3554f3a195bde4911bf6847eaa701a5
Franco Brizuela <franco_brizuela@hotmail.com>
2021-11-26 17:33:57 -0300

    Modularizado App.js

--- App.js
+++ App.js
@@ -1,60 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './Redux/Store.js'
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useNavigation } from '@react-navigation/native';
-
-//----------IMP SCREENS-----------
-import Home from './Screens/Home.js'
-import AddRestoScreen from './Screens/RegisterResto.js';
-import AddUserScreen from './Screens/RegisterUser.js';
-import AddMenuRestoScreen from './Screens/menuResto.js'
-import Nav from "./Screens/Nav.js"
-
-//-----------HELPERS-----------
-import Btn from "./Screens/Helpers/Btns.js";
-
-const Stack = createNativeStackNavigator();
+import Navigator from './routes/Navigator.js';
 
 export default function App() {
-
-
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name={"Resto Book"}
-            component={Home}
-            options={({ navigation }) => ({
-              headerTitle: () => <Nav title="Resto Book" navigation={navigation} />,
-            })}
-          />
-          <Stack.Screen
-            name="Register User"
-            component={AddUserScreen}
-          />
-          <Stack.Screen
-            name="Register Resto"
-            component={AddRestoScreen}
-          />
-          <Stack.Screen
-            name="Menu Resto"
-            component={AddMenuRestoScreen}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+        <Navigator/>
     </Provider>
   );
-
-
 }
-
-const styles = StyleSheet.create({
-
-
-});
---
+++ routes/Navigator.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+//----------IMP SCREENS-----------
+import Home from '../Screens/Home'
+import AddRestoScreen from '../Screens/RegisterResto.js';
+import AddUserScreen from '../Screens/RegisterUser.js';
+import AddMenuRestoScreen from '../Screens/menuResto.js'
+import { DetailsResto } from "../Screens/DetailsResto";
+import Nav from "../Screens/Nav.js"
+
+export default Navigator = () => {
+    const Stack = createNativeStackNavigator();
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name={"Resto Book"}
+                    component={Home}
+                    options={({ navigation }) => ({
+                    headerTitle: () => <Nav title="Resto Book" navigation={navigation} />,
+                    })}
+                />
+                <Stack.Screen
+                    name="Register User"
+                    component={AddUserScreen}
+                />
+                <Stack.Screen
+                    name="Register Resto"
+                    component={AddRestoScreen}
+                />
+                <Stack.Screen
+                    name="Menu Resto"
+                    component={AddMenuRestoScreen}
+                />
+                <Stack.Screen
+                    name="Details Resto"
+                    component={DetailsResto}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
