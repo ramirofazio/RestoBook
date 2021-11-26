655427c6cef7131bd926b65ffc6da9f00fb03cd9
unknown <lucasmira443@gmail.com>
2021-11-25 22:03:25 -0300

    RestoForm terminado, falta css

--- screens/registerResto.js
+++ screens/registerResto.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {Button,View,TextInput,ScrollView,} from "react-native";
+import {Button,View,TextInput,ScrollView, StyleSheet} from "react-native";
 
 import firebase from "../database/firebase";
 
@@ -106,5 +106,28 @@ const AddRestoScreen = (props) => {
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 35,
+    },
+    inputGroup: {
+      flex: 1,
+      padding: 0,
+      marginBottom: 15,
+      borderBottomWidth: 1,
+      borderBottomColor: "#cccccc",
+    },
+    loader: {
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+      position: "absolute",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
 
 export default AddRestoScreen;
