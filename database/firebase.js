74e31785a1d9ad100e2afa23045b99f8824fb3b6
Jorge Martin Galliano <martin.galliano@hotmail.com>
2021-11-30 16:34:35 -0300

    firebase

--- database/firebase.js
+++ database/firebase.js
@@ -1,6 +1,3 @@
-/* // ESTA ES LA CONEXIÓN A FIREBASE BASADO EN EL VIDEO DE FAZT PERO SE AÑADE LAS LÍNEAS 3,4,5,22,24
-
-// Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
@@ -30,33 +27,3 @@ export default {
   db,
   fireAuth,
 };
- */
-// ESTO SERÍA LA CONEXIÓN A FIREBASE ACTUALIZADA PERO COMO NO FUNCIONA VOY A COLOCAR EL DEL VIDEO DE FAZT
-
-// // Import the functions you need from the SDKs you need
-  import { initializeApp } from "firebase/app";
-  import { getFirestore } from "firebase/firestore";
-// import { getAnalytics } from "firebase/analytics";
-
-
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAZBmKgl0g3uGfuz9AjyBqgt884Em35EtE",
-   authDomain: "henry-restobook.firebaseapp.com",
-  projectId:"henry-restobook",
-   storageBucket:"henry-restobook.appspot.com",
-  messagingSenderId: "96924806240",
-  appId: "1:96924806240:web:9bd43698b9913454a3a705",
- measurementId: "G-Y6E3NYDYVW"
- };
-
-// // Initialize Firebase
- const firebaseApp = initializeApp(firebaseConfig);
- const db = getFirestore(firebaseApp);
-// const analytics = getAnalytics(firebaseApp);
-
-export default db;
