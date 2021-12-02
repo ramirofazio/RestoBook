3a6e6124dce71324d23d44ed9b38ecbb0d76a25c
Ramiro Fazio <ramifazio@gmail.com>
2021-12-02 03:44:04 -0300

    GlobalLogin con Formik, AddMenuResto con Formik

--- Screens/AddMenuResto.js
+++ Screens/AddMenuResto.js
@@ -1,128 +1,161 @@
 //----------REACT UTILS-----------
 import React, { useEffect, useState } from "react";
 //
-//
 //----------REDUX UTILS-----------
 import { useDispatch, useSelector } from "react-redux";
 import AddMenu from "../Redux/Actions/AddMenu";
 //
-//
 //----------REACT-NATIVE UTILS-----------
 import {
-  Button,
   View,
   TextInput,
-  ScrollView,
-  StyleSheet,
   TouchableOpacity,
   Text,
+  Image
 } from "react-native";
-//
 //
 //----------FIREBASE UTILS-----------
 import firebase from "../database/firebase";
 import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
 //
-//
 //---------SCREENS & COMPONENTS---------------
 
-//
 //
 //-------STYLES-------
 import globalStyles from "./GlobalStyles";
 //
-//
 //-------INITIALIZATIONS-------
 
 //
+//-------FORMIK------------
+import { Formik } from 'formik';
+//
+//-------IMAGE PICKER----------
+import * as ImagePicker from 'expo-image-picker';
+//
+//-------YUP(Validacion)------
+import * as yup from 'yup';
+//
 //---------------------------------------------------------------------------------------//
 //
+
+const MenuRestoSchema = yup.object({
+  foodName: yup.string()
+    .required()
+    .min(3)
+    .max(15),
+  description: yup.string()
+    .required()
+    .min(5)
+    .max(100),
+  price: yup.number()
+    .required()
+    .positive()
+    .integer()
+    .max(2000),
+})
+
 const AddMenuResto = ({ navigation }) => {
 
-  useEffect(() => {
-
-  })
-
-  const dispatch = useDispatch();
   const empresaDetail = useSelector((state) => state.empresaDetail);
-  const menu = useSelector((state) => state.empresaDetail.menu);
-
   const idResto = empresaDetail.idResto;
 
-  const initalState = {
-    foodName: "",
-    description: "",
-    price: "",
-    img: "",
-  };
-
-  const [state, setState] = useState(initalState);
-
-  const handleChangeText = (value, name) => {
-    setState({ ...state, [name]: value });
-  };
-
-  const saveMenuResto = async () => {
-    try {
-      let restoRef = doc(firebase.db, "Restos", idResto);
-      await updateDoc(restoRef, {
-        menu: arrayUnion(state)
-      })
-      navigation.navigate("DetailsResto");
-    } catch (err) {
-      console.log(err);
+  const handleOnPressPickImage = async (handleChange) => {
+    if (Platform.OS !== 'web') {
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
     }
-  };
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.cancelled) {
+      handleChange(result.uri)
+    }
+  }
 
   return (
     <View style={globalStyles.Home}>
-      <View style={globalStyles.inputContainer}>
-        <View style={globalStyles.inputComponent}>
-          <TextInput
-            style={globalStyles.texts}
-            placeholder="Food Name"
-            onChangeText={(value) => handleChangeText(value, "foodName")}
-            value={state.foodName}
-          />
-        </View>
-        <View style={globalStyles.inputComponent}>
-          <TextInput
-            style={globalStyles.texts}
-            placeholder="Description"
-            onChangeText={(value) => handleChangeText(value, "description")}
-            value={state.description}
-          />
-        </View>
-        <View style={globalStyles.inputComponent}>
-          <TextInput
-            style={globalStyles.texts}
-            placeholder="Price"
-            onChangeText={(value) => handleChangeText(value, "price")}
-            value={state.price}
-          />
-        </View>
-        <View style={globalStyles.inputComponent}>
-          <TextInput
-            style={globalStyles.texts}
-            placeholder="Image"
-            onChangeText={(value) => handleChangeText(value, "img")}
-            value={state.img}
-          />
-        </View>
-      </View>
-      <View style={globalStyles.container}>
-        {/* <Button title="Add Food" onPress={() => saveMenuResto()} /> */}
-        <TouchableOpacity
-          style={globalStyles.touchLog}
-          onPress={() => saveMenuResto()}
-        >
-          <Text style={globalStyles.fontLog}>Add Food</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-};
+      <Formik
+        initialValues={{
+          foodName: "", description: "", price: "", img: "",
+        }}
+        validationSchema={MenuRestoSchema}
+        onSubmit={async (values) => {
+          try {
+            let restoRef = doc(firebase.db, "Restos", idResto);
+            await updateDoc(restoRef, {
+              menu: arrayUnion(values)
+            })
+            navigation.navigate("DetailsResto");
+          } catch (err) {
+            console.log(err);
+          }
+        }}
+      >
+        {(props) => (
+          <View style={globalStyles.inputContainer}>
+            <View style={globalStyles.inputComponent}>
+              <TextInput
+                style={globalStyles.texts}
+                placeholder="Food Name"
+                onChangeText={props.handleChange("foodName")}
+                value={props.values.foodName}
+                onBlur={props.handleBlur("foodName")}
+              />
+            </View>
+            {props.touched.foodName && props.errors.foodName ? <Text>{props.errors.foodName}</Text> : null}
+            <View style={globalStyles.inputComponent}>
+              <TextInput
+                multiline
+                style={globalStyles.texts}
+                placeholder="description"
+                onChangeText={props.handleChange("description")}
+                value={props.values.description}
+                onBlur={props.handleBlur("description")}
 
-const styles = StyleSheet.create({});
+              />
+            </View>
+            {props.touched.description && props.errors.description ? <Text>{props.errors.description}</Text> : null}
+            <View style={globalStyles.inputComponent}>
+              <TextInput
+                style={globalStyles.texts}
+                placeholder="price"
+                onChangeText={props.handleChange("price")}
+                value={props.values.price}
+                keyboardType="numeric"
+                onBlur={props.handleBlur("price")}
+              />
+            </View>
+            {props.touched.price && props.errors.price ? <Text>{props.errors.price}</Text> : null}
+            <TouchableOpacity
+              style={globalStyles.touchLog}
+              onPress={() => { handleOnPressPickImage(props.handleChange("img")) }}
+            >
+              <Text style={{ textAlign: "center" }}>
+                {props.values.img && props.values.img.length > 0 ? "Change Image" : "Select Image"}
+              </Text>
+            </TouchableOpacity>
+            {props.values.img && props.values.img.length > 0 ?
+              <Image source={{ uri: props.values.img }} style={{ width: 200, height: 200, borderRadius: 15 }} /> : null}
+            <View>
+              <TouchableOpacity
+                style={globalStyles.touchLog}
+                onPress={() => props.handleSubmit()}
+              >
+                <Text style={globalStyles.fontLog}>Add Food</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      </Formik>
+    </View >
+  )
+}
 
 export default AddMenuResto;
--- Screens/GlobalLogin.js
+++ Screens/GlobalLogin.js
@@ -40,7 +40,7 @@ import firebase from "../database/firebase";
 //
 //
 //-------ICONS-------
-//import { MaterialCommunityIcons as Icon } from "@expo/vector-icons"; //SE BORRA?
+import { MaterialCommunityIcons as Icon } from "@expo/vector-icons"; //SE BORRA?
 //
 //
 //-------STYLES-------
@@ -51,185 +51,163 @@ import globalStyles from "./GlobalStyles";
 const auth = getAuth();
 const googleProvider = new GoogleAuthProvider();
 //
+//-------YUP(Validacion)------
+import * as yup from 'yup';
+//
 //---------------------------------------------------------------------------------------//
 //
+
+const GlobalLoginSchema = yup.object({
+  email: yup.string()
+    .required()
+    .email(),
+  password: yup.string()
+    .required()
+    .min(5)
+    .max(12)
+})
+
 export default GlobalLogin = ({ navigation }) => {
-  const [user, setUser] = useState({
-    mail: "",
-    password: "",
-    secureTextEntry: true,
-    iconName: "eye",
-  });
-  const [registered, setRegistered] = useState(true);
 
-  const email = user.mail;
-  const pass = user.password;
+  // const secureTextEntry = (handleChange) => {
+  //   handleChange(false)
+  // }
 
-  const handleChangeUser = (field, value) => {
-    setUser({
-      ...user,
-      [field]: value,
-    });
-  };
+  const [flagLoginOrRegister, setFlagLoginOrRegister] = useState(true)
+  const [flagSecureText, setFlagSecureText] = useState(true)
 
-  // const logUserWithGoogle = async () => {
-  //   try {
-  //     const newUser = await signInWithRedirect(auth, googleProvider);
-  //     if (auth.currentUser) {
-  //       console.log("works");
-  //       props.navigation.navigate("RestoBook");
-  //     } else {
-  //       console.log("NO works");
-  //     }
-  //   } catch (error) {
-  //     alert("error!");
-  //     console.log(error);
-  //   }
-  // };
-
-  const logEmpresa = async () => {
-    try {
-      const newUser = await signInWithEmailAndPassword(auth, email, pass);
-      if (auth.currentUser.emailVerified) {
-        alert("Welcome");
-        navigation.navigate("RestoBook");
-      } else {
-        navigation.navigate("AwaitEmail");
-      }
-    } catch (error) {
-      const errorCode = error.code;
-      console.log("errorCode", errorCode);
-      switch (errorCode) {
-        case "auth/wrong-password":
-          alert("Wrong password");
-          break;
-        case "auth/user-not-found":
-          alert("User not found");
-          break;
-        case "auth/internal-error":
-          alert("Enter your password!");
-          break;
-        default:
-          alert("Error");
-      }
-    }
-  };
-
-  const saveEmpresa = async () => {
-    try {
-      await createUserWithEmailAndPassword(auth, email, pass);
-      onAuthStateChanged(auth, (usuarioFirebase) => {
-        if (usuarioFirebase) {
-          sendEmailVerification(auth.currentUser)
-            .then(handleChangeUser("mail", ""))
-            .then(handleChangeUser("password", ""))
-            .then(alert("Sign Up!"))
-            .then(navigation.navigate("AwaitEmail"));
-        }
-      });
-    } catch (error) {
-      const errorCode = error.code;
-      console.log("errorCode", errorCode);
-      switch (errorCode) {
-        case "auth/invalid-email":
-          alert("Invalid Email");
-          break;
-        case "auth/weak-password":
-          alert("password must be at least 6 characters");
-          break;
-        case "auth/email-already-in-use":
-          alert("Email already in-use");
-          break;
-        default:
-          break;
-      }
-    }
-  };
-
-  const buttonText = [
-    "Log In",
-    "Sign Up",
-    "I don't have an account yet",
-    "I already have an account",
-  ];
-
-  const onIconPress = () => {
-    let iconName = user.secureTextEntry ? "eye-off" : "eye";
-    setUser({
-      ...user,
-      secureTextEntry: !user.secureTextEntry,
-      iconName: iconName,
-    });
-  };
-
-  return (
-    <View style={globalStyles.Home}>
-      <ScrollView contentContainerStyle={{ flex: 1 }}>
-        <View style={globalStyles.inputContainer}>
-          <View style={globalStyles.inputComponent}>
-            <TextInput
-              style={globalStyles.texts}
-              placeholder="Email"
-              value={user.mail}
-              onChangeText={(value) => handleChangeUser("mail", value)}
-            />
-          </View>
-          <View style={globalStyles.inputComponent}>
-            <TextInput
-              style={globalStyles.texts}
-              onPress={onIconPress}
-              secureTextEntry={user.secureTextEntry}
-              placeholder="Password"
-              value={user.password}
-              onChangeText={(value) => handleChangeUser("password", value)}
-            />
-            {/* <View>
-              <TouchableOpacity
-                onPress={onIconPress}
-                style={styles.inputComponent}
-              >
-                <Icon name={user.iconName} size={20} />
-              </TouchableOpacity>
-            </View> */}
-          </View>
-        </View>
-
-        <View style={globalStyles.container}>
-          <TouchableOpacity
-            style={globalStyles.touchLog}
-            onPress={() => logEmpresa()}
-          >
-            <Text style={globalStyles.fontLog}>Log In</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={globalStyles.touchLog}
-            onPress={() => saveEmpresa()}
-          >
-            <Text style={globalStyles.fontLog}>Sign Up</Text>
-          </TouchableOpacity>
-          {/* <TouchableOpacity
-            style={globalStyles.touchFlag}
-            onPress={() =>
-              registered ? setRegistered(false) : setRegistered(true)
+  if (flagLoginOrRegister) {
+    return (
+      //------------LOGIN---------------
+      <View style={globalStyles.Home}>
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+          validationSchema={GlobalLoginSchema}
+          onSubmit={async ({ email, password }) => {
+            try {
+              const newUser = await signInWithEmailAndPassword(auth, email, password);
+              if (auth.currentUser.emailVerified) {
+                alert("Welcome");
+                navigation.navigate("RestoBook");
+              } else {
+                navigation.navigate("AwaitEmail")
+              }
+            } catch (err) {
+              alert(err)
             }
-          >
-            <Text style={globalStyles.fontLog}>
-              {registered ? buttonText[2] : buttonText[3]}
-            </Text>
-          </TouchableOpacity> */}
-
-          {/* <Text>O</Text>
-          <TouchableOpacity
-            style={globalStyles.touchLog}
-            onPress={() => logUserWithGoogle()}
-          >
-            <Text style={globalStyles.fontLog}>Sign In With Google</Text>
-          </TouchableOpacity> */}
-        </View>
-      </ScrollView>
-    </View>
-  );
+          }}
+        >
+          {(props) => (
+            <View style={globalStyles.inputContainer}>
+              <View style={globalStyles.inputComponent}>
+                <TextInput
+                  style={globalStyles.texts}
+                  placeholder="Email"
+                  onChangeText={props.handleChange("email")}
+                  value={props.values.email}
+                  onBlur={props.handleBlur("email")}
+                />
+              </View>
+              {props.touched.email && props.errors.email ? <Text>{props.errors.email}</Text> : null}
+              <View style={globalStyles.inputComponent}>
+                <TextInput
+                  style={globalStyles.texts}
+                  placeholder="password"
+                  onChangeText={props.handleChange("password")}
+                  value={props.values.password}
+                  secureTextEntry={flagSecureText}
+                  onBlur={props.handleBlur("password")}
+                />
+              </View>
+              {props.touched.password && props.errors.password ? <Text>{props.errors.password}</Text> : null}
+              <TouchableOpacity
+                onPress={() => flagSecureText ? setFlagSecureText(false) : setFlagSecureText(true)}
+              >
+                <Icon name={flagSecureText ? "eye-off" : "eye"} size={20} />
+              </TouchableOpacity>
+              <View style={globalStyles.container}>
+                <TouchableOpacity
+                  style={globalStyles.touchLog}
+                  onPress={() => props.handleSubmit()}
+                >
+                  <Text style={globalStyles.fontLog}>Log In</Text>
+                </TouchableOpacity>
+                <Text onPress={() => setFlagLoginOrRegister(false)}>I dont have an account yet</Text>
+              </View>
+            </View >
+          )}
+        </Formik >
+      </View >
+    )
+  } else {
+    return (
+      //-------REGISTER-------------
+      <View style={globalStyles.Home}>
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+          validationSchema={GlobalLoginSchema}
+          onSubmit={async ({ email, password }) => {
+            try {
+              await createUserWithEmailAndPassword(auth, email, password);
+              onAuthStateChanged(auth, (usuarioFirebase) => {
+                if (usuarioFirebase) {
+                  sendEmailVerification(auth.currentUser)
+                    .then(alert("Sign Up!"))
+                    .then(navigation.navigate("AwaitEmail"));
+                }
+              });
+            } catch (err) {
+              alert(err)
+            }
+          }}
+        >
+          {(props) => (
+            <View style={globalStyles.inputContainer}>
+              <View style={globalStyles.inputComponent}>
+                <TextInput
+                  style={globalStyles.texts}
+                  placeholder="Email"
+                  onChangeText={props.handleChange("email")}
+                  value={props.values.email}
+                  onBlur={props.handleBlur("email")}
+                />
+              </View>
+              {props.touched.email && props.errors.email ? <Text>{props.errors.email}</Text> : null}
+              <View style={globalStyles.inputComponent}>
+                <TextInput
+                  style={globalStyles.texts}
+                  placeholder="password"
+                  onChangeText={props.handleChange("password")}
+                  value={props.values.password}
+                  secureTextEntry={flagSecureText}
+                  onBlur={props.handleBlur("password")}
+                />
+              </View>
+              {props.touched.password && props.errors.password ? <Text>{props.errors.password}</Text> : null}
+              <TouchableOpacity
+                onPress={() => flagSecureText ? setFlagSecureText(false) : setFlagSecureText(true)}
+              >
+                <Icon name={flagSecureText ? "eye-off" : "eye"} size={20} />
+              </TouchableOpacity>
+              <View style={globalStyles.container}>
+                <TouchableOpacity
+                  style={globalStyles.touchLog}
+                  onPress={() => props.handleSubmit()}
+                >
+                  <Text style={globalStyles.fontLog}>Sign Up</Text>
+                </TouchableOpacity>
+                <Text onPress={() => setFlagLoginOrRegister(true)}>I have an account</Text>
+              </View>
+            </View >
+          )}
+        </Formik >
+      </View>
+    )
+  };
 };
-
-  
