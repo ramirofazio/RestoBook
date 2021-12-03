6c6f17246f1f388afe76c67e10d5f549aff802aa
Ramiro Fazio <ramifazio@gmail.com>
2021-12-03 12:31:20 -0300

    GlobalLogin Modal y un poco de CSS

--- Screens/GlobalLogin.js
+++ Screens/GlobalLogin.js
@@ -64,6 +64,29 @@ const GlobalLoginSchema = yup.object({
     .max(12)
 })
 
+const GlobalRegisterSchema = yup.object({
+  name: yup.string()
+    .required(),
+  lastName: yup.string()
+    .required(),
+  cel: yup.number()
+    .required(),
+  email: yup.string()
+    .required()
+    .email(),
+  password: yup.string()
+    .required()
+    .min(6)
+    .max(12),
+  passwordConfirm: yup.string()
+    .required()
+    .test("password-match",
+      "passwords must match",
+      async (value, testContext) => testContext.parent.password === value,
+    )
+})
+
+
 const GlobalLogin = ({ navigation }) => {
 
   // const secureTextEntry = (handleChange) => {
@@ -72,12 +95,12 @@ const GlobalLogin = ({ navigation }) => {
 
   const [flagLoginOrRegister, setFlagLoginOrRegister] = useState(true)
   const [flagSecureText, setFlagSecureText] = useState(true)
-  const [modalVisible, setModalVisible] = useState(false);
 
   if (flagLoginOrRegister) {
     return (
       //------------LOGIN---------------
       <View style={globalStyles.Home}>
+        <Text style={globalStyles.componentTitle}>Login</Text>
         <Formik
           initialValues={{
             email: "",
@@ -109,7 +132,7 @@ const GlobalLogin = ({ navigation }) => {
                   onBlur={props.handleBlur("email")}
                 />
               </View>
-              {props.touched.email && props.errors.email ? <Text>{props.errors.email}</Text> : null}
+              {props.touched.email && props.errors.email ? <Text style={globalStyles.errorText}>{props.errors.email}</Text> : null}
               <View style={globalStyles.inputComponent}>
                 <TextInput
                   style={globalStyles.texts}
@@ -120,8 +143,9 @@ const GlobalLogin = ({ navigation }) => {
                   onBlur={props.handleBlur("password")}
                 />
               </View>
-              {props.touched.password && props.errors.password ? <Text>{props.errors.password}</Text> : null}
+              {props.touched.password && props.errors.password ? <Text style={globalStyles.errorText}>{props.errors.password}</Text> : null}
               <TouchableOpacity
+                style={globalStyles.eye}
                 onPress={() => flagSecureText ? setFlagSecureText(false) : setFlagSecureText(true)}
               >
                 <Icon name={flagSecureText ? "eye-off" : "eye"} size={20} />
@@ -133,42 +157,7 @@ const GlobalLogin = ({ navigation }) => {
                 >
                   <Text style={globalStyles.fontLog}>Log In</Text>
                 </TouchableOpacity>
-                <Text onPress={() => setModalVisible(true)}>I dont have an account yet</Text>
-                {/* ------------------------- modal ---------------------------------  */}
-                <Modal
-                  animationType="slide"
-                  transparent={true}
-                  visible={modalVisible}
-                  onRequestClose={() => {
-                    Alert.alert("Modal has been closed.");
-                    setModalVisible(!modalVisible);
-                  }}
-                >
-                  <View style={styles.centeredView}>
-                    <View style={styles.modalView}>
-                      <TouchableOpacity
-                        style={globalStyles.touchLog}
-                        onPress={() => setModalVisible(!modalVisible)}
-                      >
-                        <Text style={styles.textStyle}>X</Text>
-                      </TouchableOpacity>
-                      <Text style={styles.modalText}>Welcome to Restobook</Text>
-
-                      <TextInput
-                        style={globalStyles.texts}
-                        placeholder="Email"
-                      />
-
-                      <TouchableOpacity
-                        style={globalStyles.touchLog}
-
-                      >
-                        <Text>Check in</Text>
-                      </TouchableOpacity>
-                    </View>
-                  </View>
-                </Modal>
-                {/* ----------------------------------------------------------------------------------- */}
+                <Text style={globalStyles.textDownButton} onPress={() => setFlagLoginOrRegister(false)}>I dont have an account yet</Text>
               </View>
             </View >
           )}
@@ -176,161 +165,150 @@ const GlobalLogin = ({ navigation }) => {
       </View >
     )
   } else {
-    const GlobalRegisterSchema = yup.object({
-      name: yup.string()
-        .required(),
-      lastName: yup.string()
-        .required(),
-      cel: yup.number()
-        .required(),
-      email: yup.string()
-        .required()
-        .email(),
-      password: yup.string()
-        .required()
-        .min(6)
-        .max(12),
-      passwordConfirm: yup.string()
-        .required()
-        .test("password-match",
-          "passwords must match",
-          async (value, testContext) => testContext.parent.password === value,
-        )
-    })
-
     return (
       //-------REGISTER-------------
-      <View style={globalStyles.Home}>
-        <Formik
-          initialValues={{
-            name: "",
-            lastName: "",
-            cel: "",
-            email: "",
-            password: "",
-            passwordConfirm: "",
-          }}
-          validationSchema={GlobalRegisterSchema}
-          onSubmit={async (values) => {
-            console.log(values)
-            try {
-              //-----AUTENTICA USER-----------
-              await createUserWithEmailAndPassword(auth, values.email, values.password);
-              onAuthStateChanged(auth, (usuarioFirebase) => {
-                if (usuarioFirebase) {
-                  //-----AGREGA A COLECCION USER--------
-                  firebase.db.collection("Users").doc(auth.currentUser.uid).set({
-                    id: auth.currentUser.uid,
-                    name: values.name,
-                    lastName: values.lastName,
-                    cel: values.cel,
-                    email: values.email,
-                    commerce: false,
-                    reservations: [],
-                    payments: [],
-                  })
-                    .then(sendEmailVerification(auth.currentUser))
-                    .then(navigation.navigate("AwaitEmail"));
-                }
-              });
-            } catch (err) {
-              alert(err)
-            }
-          }}
+      <View>
+        <Modal
+          animationType="slide"
+          transparent={true}
         >
-          {(props) => (
-            <View style={globalStyles.inputContainer}>
-              <View style={globalStyles.inputComponent}>
-                <TextInput
-                  style={globalStyles.texts}
-                  placeholder="Nombre"
-                  onChangeText={props.handleChange("name")}
-                  value={props.values.name}
-                  onBlur={props.handleBlur("name")}
-                />
-              </View>
-              {props.touched.name && props.errors.name ? <Text>{props.errors.name}</Text> : null}
-              <View style={globalStyles.inputComponent}>
-                <TextInput
-                  style={globalStyles.texts}
-                  placeholder="Apellido"
-                  onChangeText={props.handleChange("lastName")}
-                  value={props.values.lastName}
-                  onBlur={props.handleBlur("lastName")}
-                />
-              </View>
-              {props.touched.lastName && props.errors.lastName ? <Text>{props.errors.lastName}</Text> : null}
-              <View style={globalStyles.inputComponent}>
-                <TextInput
-                  style={globalStyles.texts}
-                  placeholder="Telephone"
-                  onChangeText={props.handleChange("cel")}
-                  value={props.values.cel}
-                  onBlur={props.handleBlur("cel")}
-                  keyboardType="numeric"
-                />
-              </View>
-              {props.touched.cel && props.errors.cel ? <Text>{props.errors.cel}</Text> : null}
-              <View style={globalStyles.inputComponent}>
-                <TextInput
-                  style={globalStyles.texts}
-                  placeholder="Email"
-                  onChangeText={props.handleChange("email")}
-                  value={props.values.email}
-                  onBlur={props.handleBlur("email")}
-                />
-              </View>
-              {props.touched.email && props.errors.email ? <Text>{props.errors.email}</Text> : null}
-              <View style={globalStyles.inputComponent}>
-                <TextInput
-                  style={globalStyles.texts}
-                  placeholder="password"
-                  onChangeText={props.handleChange("password")}
-                  value={props.values.password}
-                  secureTextEntry={flagSecureText}
-                  onBlur={props.handleBlur("password")}
-                />
-              </View>
-              {props.touched.password && props.errors.password ? <Text>{props.errors.password}</Text> : null}
-              <View style={globalStyles.inputComponent}>
-                <TextInput
-                  style={globalStyles.texts}
-                  placeholder="Confirm password"
-                  onChangeText={props.handleChange("passwordConfirm")}
-                  value={props.values.passwordConfirm}
-                  secureTextEntry={flagSecureText}
-                  onBlur={props.handleBlur("passwordConfirm")}
-                />
-              </View>
-              {props.touched.passwordConfirm && props.errors.passwordConfirm ? <Text>{props.errors.passwordConfirm}</Text> : null}
-              <TouchableOpacity
-                onPress={() => flagSecureText ? setFlagSecureText(false) : setFlagSecureText(true)}
-              >
-                <Icon name={flagSecureText ? "eye-off" : "eye"} size={20} />
-              </TouchableOpacity>
-              <View style={globalStyles.container}>
-                <TouchableOpacity
-                  style={globalStyles.touchLog}
-                  onPress={() => props.handleSubmit()}
-                >
-                  <Text style={globalStyles.fontLog}>Sign Up</Text>
-                </TouchableOpacity>
-                <Text onPress={() => setFlagLoginOrRegister(true)}>I have an account</Text>
+          <View View style={globalStyles.Home} >
+            <Formik
+              initialValues={{
+                name: "",
+                lastName: "",
+                cel: "",
+                email: "",
+                password: "",
+                passwordConfirm: "",
+              }}
+              validationSchema={GlobalRegisterSchema}
+              onSubmit={async (values) => {
+                console.log(values)
+                try {
+                  //-----AUTENTICA USER-----------
+                  await createUserWithEmailAndPassword(auth, values.email, values.password);
+                  onAuthStateChanged(auth, (usuarioFirebase) => {
+                    if (usuarioFirebase) {
+                      //-----AGREGA A COLECCION USER--------
+                      firebase.db.collection("Users").doc(auth.currentUser.uid).set({
+                        id: auth.currentUser.uid,
+                        name: values.name,
+                        lastName: values.lastName,
+                        cel: values.cel,
+                        email: values.email,
+                        commerce: false,
+                        reservations: [],
+                        payments: [],
+                      })
+                        .then(sendEmailVerification(auth.currentUser))
+                        .then(navigation.navigate("AwaitEmail"));
+                    }
+                  });
+                } catch (err) {
+                  alert(err)
+                }
+              }}
+            >
+              {(props) => (
+                <View style={globalStyles.modalInputContainer}>
+                  <Text style={styles.modalText}>Register to RestoBook</Text>
 
-              </View>
-            </View >
-          )}
-        </Formik >
-      </View >
+
+                  <View style={globalStyles.inputComponent}>
+                    <TextInput
+                      style={globalStyles.texts}
+                      placeholder="Nombre"
+                      onChangeText={props.handleChange("name")}
+                      value={props.values.name}
+                      onBlur={props.handleBlur("name")}
+                    />
+                  </View>
+                  {props.touched.name && props.errors.name ? <Text style={globalStyles.errorText}>{props.errors.name}</Text> : null}
+                  <View style={globalStyles.inputComponent}>
+                    <TextInput
+                      style={globalStyles.texts}
+                      placeholder="Apellido"
+                      onChangeText={props.handleChange("lastName")}
+                      value={props.values.lastName}
+                      onBlur={props.handleBlur("lastName")}
+                    />
+                  </View>
+                  {props.touched.lastName && props.errors.lastName ? <Text style={globalStyles.errorText}>{props.errors.lastName}</Text> : null}
+                  <View style={globalStyles.inputComponent}>
+                    <TextInput
+                      style={globalStyles.texts}
+                      placeholder="Telephone"
+                      onChangeText={props.handleChange("cel")}
+                      value={props.values.cel}
+                      onBlur={props.handleBlur("cel")}
+                      keyboardType="numeric"
+                    />
+                  </View>
+                  {props.touched.cel && props.errors.cel ? <Text style={globalStyles.errorText}>{props.errors.cel}</Text> : null}
+                  <View style={globalStyles.inputComponent}>
+                    <TextInput
+                      style={globalStyles.texts}
+                      placeholder="Email"
+                      onChangeText={props.handleChange("email")}
+                      value={props.values.email}
+                      onBlur={props.handleBlur("email")}
+                    />
+                  </View>
+                  {props.touched.email && props.errors.email ? <Text style={globalStyles.errorText}>{props.errors.email}</Text> : null}
+                  <View style={globalStyles.inputComponent}>
+                    <TextInput
+                      style={globalStyles.texts}
+                      placeholder="password"
+                      onChangeText={props.handleChange("password")}
+                      value={props.values.password}
+                      secureTextEntry={flagSecureText}
+                      onBlur={props.handleBlur("password")}
+                    />
+                  </View>
+                  {props.touched.password && props.errors.password ? <Text style={globalStyles.errorText}>{props.errors.password}</Text> : null}
+                  <View style={globalStyles.inputComponent}>
+                    <TextInput
+                      style={globalStyles.texts}
+                      placeholder="Confirm password"
+                      onChangeText={props.handleChange("passwordConfirm")}
+                      value={props.values.passwordConfirm}
+                      secureTextEntry={flagSecureText}
+                      onBlur={props.handleBlur("passwordConfirm")}
+                    />
+                  </View>
+                  {props.touched.passwordConfirm && props.errors.passwordConfirm ? <Text style={globalStyles.errorText}>{props.errors.passwordConfirm}</Text> : null}
+                  <TouchableOpacity
+                    style={globalStyles.eye}
+                    onPress={() => flagSecureText ? setFlagSecureText(false) : setFlagSecureText(true)}
+                  >
+                    <Icon name={flagSecureText ? "eye-off" : "eye"} size={20} />
+                  </TouchableOpacity>
+                  <View style={globalStyles.container}>
+                    <TouchableOpacity
+                      style={globalStyles.touchLog}
+                      onPress={() => props.handleSubmit()}
+                    >
+                      <Text style={globalStyles.fontLog}>Sign Up</Text>
+                    </TouchableOpacity>
+                    <Text style={globalStyles.textDownButton} onPress={() => setFlagLoginOrRegister(true)}>I have an account</Text>
+
+                  </View>
+                </View >
+              )}
+            </Formik >
+          </View >
+        </Modal>
+      </View>
     )
   };
-};
+}
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 22,
+    marginTop: 70,
     //backgroundColor: "blur",
   },
   modalView: {
@@ -369,7 +347,7 @@ const styles = StyleSheet.create({
   modalText: {
     marginBottom: 15,
     textAlign: "center",
-    fontSize: 30,
+    fontSize: 25,
     fontWeight: "bold",
   },
   botton: {
--- Screens/GlobalStyles.js
+++ Screens/GlobalStyles.js
@@ -4,115 +4,143 @@ import { StyleSheet } from "react-native";
 
 
 const globalStyles = StyleSheet.create({
-    Home: {
-        flex: 1,
-        backgroundColor: '#ffdfcb'
-    },
-    btnHome: {
-        marginTop: 10,
-        flexDirection: "row",
-        justifyContent: "space-around"
-    },
-    btnText: {
-        fontSize: 12.5,
-        color: "#392c28",
-    },
-    btnContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-around",
-        width: 160,
-    },
-    Container: {
-        alignItems: "center",
-    },
-    btn: {
-        backgroundColor: '#ffd964',
-        paddingVertical: 8,
-        paddingHorizontal: 9,
-        borderRadius: 10,
-        alignItems: "center",
-        borderWidth: 1,
-        borderColor: '#b39138',
-    },
-    img: {
-        height: 40,
-        width: 40,
-        marginTop: 5,
-        alignItems: "center",
-        resizeMode: 'contain' // esta linea es para que se adapte al tam;o de la imagen
-    },
-    title: {
-        fontSize: 25,
-        fontWeight: "bold",
-        paddingVertical: 10,
-        paddingHorizontal: 5,
-        color: '#392c28'
-    },
-    containerTitle: {
-        flexDirection: "row",
-    },
-    navHome: {
-        flexDirection: "row",
-        width: "100%",
-    },
-    touchLog: {
-        marginTop: 10,
-        maxWidth: "100%",
-        width: '50%',
-        alignItems: "center",
-        borderRadius: 10,
-        backgroundColor: "#bd967e",
-        padding: 10,
-      },
-      touchFlag: {
-        marginTop: 10,
-        maxWidth: "100%",
-        width: '50%',
-        alignItems: "center",
-        borderRadius: 10,
-        backgroundColor: "#ffdfcb",
-        borderWidth: 2,
-        borderColor: "#bd967e",
-        padding: 10,
-      },
-      fontLog: {
-        color: "#392c28",
-        fontWeight: "bold"
-      },
-      container: {
-        flex: 6,
-        alignItems: "center",
-      },
-      text: {
-        fontSize: 20,
-        width: "100%",
-      },
-      texts: {
-        textAlign: "center",
-        width: "100%",
-        fontSize: 14.5,
-        fontWeight: "bold",
-      },
-      textContainer: {
-        flex: 1,
-        alignSelf: "center",
-        justifyContent: "center",
-        width:"70%",
-      },
-      inputContainer: {
-        flex: 2,
-        alignItems: "center",
-        justifyContent: "space-evenly",
-      },
-      inputComponent: {
-        alignSelf: "center",
-        borderRadius: 10,
-        backgroundColor: "#e8b595",
-        maxWidth: '100%',
-        width: '60%',
-        borderRadius: 10,
-      },
+  Home: {
+    flex: 1,
+    backgroundColor: '#ffdfcb'
+  },
+  btnHome: {
+    marginTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  btnText: {
+    fontSize: 12.5,
+    color: "#392c28",
+  },
+  btnContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    width: 160,
+  },
+  Container: {
+    alignItems: "center",
+  },
+  btn: {
+    backgroundColor: '#ffd964',
+    paddingVertical: 8,
+    paddingHorizontal: 9,
+    borderRadius: 10,
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: '#b39138',
+  },
+  img: {
+    height: 40,
+    width: 40,
+    marginTop: 5,
+    alignItems: "center",
+    resizeMode: 'contain' // esta linea es para que se adapte al tam;o de la imagen
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: "bold",
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    color: '#392c28'
+  },
+  componentTitle: {
+    alignSelf: "center",
+    marginTop: 10,
+    fontSize: 30,
+    color: "#000"
+
+  },
+  containerTitle: {
+    flexDirection: "row",
+  },
+  navHome: {
+    flexDirection: "row",
+    width: "100%",
+  },
+  touchLog: {
+    marginTop: 10,
+    maxWidth: "100%",
+    width: '50%',
+    alignItems: "center",
+    borderRadius: 10,
+    backgroundColor: "#bd967e",
+    padding: 10,
+  },
+  touchFlag: {
+    marginTop: 10,
+    maxWidth: "100%",
+    width: '50%',
+    alignItems: "center",
+    borderRadius: 10,
+    backgroundColor: "#ffdfcb",
+    borderWidth: 2,
+    borderColor: "#bd967e",
+    padding: 10,
+  },
+  fontLog: {
+    color: "#392c28",
+    fontWeight: "bold"
+  },
+  container: {
+    flex: 6,
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+    width: "100%",
+  },
+  texts: {
+    textAlign: "center",
+    width: "100%",
+    fontSize: 14.5,
+    fontWeight: "bold",
+  },
+  textDownButton: {
+    textAlign: "center",
+    width: "100%",
+    fontSize: 13,
+    marginTop: 5,
+  },
+  errorText: {
+    textAlign: "center",
+    width: "100%",
+    fontSize: 12,
+  },
+  textContainer: {
+    flex: 1,
+    alignSelf: "center",
+    justifyContent: "center",
+    width: "70%",
+  },
+  inputContainer: {
+    flex: 2,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  inputComponent: {
+    alignSelf: "center",
+    marginTop: 15,
+    borderRadius: 10,
+    backgroundColor: "#e8b595",
+    maxWidth: '100%',
+    width: '60%',
+    borderRadius: 10,
+  },
+  modalInputContainer: {
+    flex: 2,
+    marginTop: 50,
+    alignItems: "center",
+
+  },
+  eye: {
+    marginTop: 10,
+  },
 });
 
 export default globalStyles;
