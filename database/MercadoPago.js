9badbd7c092573d9932ed86d061709be44f2f2ca
LucasRomanGervasi <gervasilucas22@gmail.com>
2021-12-02 22:32:16 -0300

    mercadopa

---
+++ database/MercadoPago.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+// SDK de Mercado Pago
+const mercadopago = require("mercadopago");
+// Agrega credenciales
+mercadopago.configure({
+    access_token: 'TEST-6626117249048001-113019-41bd152ed3c95801fd3b94558b688947-502953409',
+});
+
+app.post('/checkout', (req, res) => {
+  let preference = {
+    external_reference: '1234567890',
+    items: [
+      {
+        title: req.body.title,
+        unit_price: 100,
+        quantity: 1,
+      },
+    ],
+    back_urls: {
+      success: 'exp://192.168.0.13:19006/success',
+      failure: 'exp:////192.168.0.13:19006/cancel'
+    },
+    auto_return: 'approved',
+  };
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      console.log(response.body);
+      res.send(response.body.sandbox_init_point);
+
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+// app.get('/success', (req, res) => {
+//   console.log('QUERYYYYY', req.query)
+//   console.log('PARAMSSSS', req.params)
+//   console.log('BODYYYYYY', req.body);
+//   res.send('Approved')
+// })
+
+// app.get('/cancel', (req, res) => {
+//   res.send('Cancel')
+// })
+// app.get('/payment', (req, res) => {
+//   const id = req.body.id;
+
+// })
+
+app.listen(process.env.PORT || 19006, () => {
+
+  console.log("Server Running");
+
+})
